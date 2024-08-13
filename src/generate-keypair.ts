6be8a8c9ea843c6a135f480d5bf206cd136e52ce
Mityasaurus <dmitriybovdun5@gmail.com>
2024-08-13 19:14:21 +0300

    refactor: delete comments

--- src/generate-keypair.ts
+++ src/generate-keypair.ts
@@ -2,6 +2,3 @@ import "dotenv/config";
 import { Keypair } from "@solana/web3.js";
 
 export const keypair = Keypair.generate();
-
-// console.log(`Public key: ${keypair.publicKey.toBase58()}`);
-// console.log(`Private key: ${keypair.secretKey}`);
--- src/load-keypair.ts
+++ src/load-keypair.ts
@@ -4,5 +4,3 @@ import { Keypair } from "@solana/web3.js";
 const asArray = Uint8Array.from(JSON.parse(process.env["SECRET_KEY"]!));
 
 export const keypair = Keypair.fromSecretKey(asArray);
-
-// console.log(`Public key: ${keypair.publicKey.toBase58()}`);
