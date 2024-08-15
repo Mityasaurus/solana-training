01d9184ad45f895ca44216fdaf8f61b43ae82d45
Mityasaurus <dmitriybovdun5@gmail.com>
2024-08-15 21:44:56 +0300

    Meeting 4

---
+++ src/create-token-mint.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { getExplorerLink } from "@solana-developers/helpers";
+import { createMint } from "@solana/spl-token";
+import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`🔑 Our public key is: ${sender.publicKey.toBase58()}`);
+
+const tokenMint = await createMint(
+  connection,
+  sender,
+  sender.publicKey,
+  null,
+  2
+);
+
+const link = getExplorerLink("address", tokenMint.toString(), "devnet");
+
+console.log(`✅ Token mint: ${link}`);
